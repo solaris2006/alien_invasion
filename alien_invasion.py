a9a88d39280e9a07609d966d9efb5dcc0166a29e
solaris2006 <cristian.stefan@gmail.com>
2021-06-17 18:11:13 +0300

    initial push

---
+++ alien_invasion.py
@@ -0,0 +1,92 @@
+import sys
+import pygame
+
+from bullet import Bullet
+from settings import  Settings
+from ship import Ship
+
+
+class AlienInvasion:
+    """Overall class to manage game assets and behaviour"""
+
+    def __init__(self) -> None:
+        """Initialize the game , and create game resources."""
+        pygame.init()
+        self.settings = Settings()
+
+        # self.screen = pygame.display.set_mode((0,0), pygame.FULLSCREEN)
+        # self.settings.screen_width = self.screen.get_rect().width
+        # self.settings.screen_height = self.screen.get_rect().height
+        self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
+        pygame.display.set_caption("Alien Invasion")
+
+        self.ship = Ship(self)
+        self.bullets = pygame.sprite.Group()
+
+
+        # Set the background color.
+        self.bg_color = (230, 230, 230)
+
+    def run_game(self):
+        """Start the main loop of the game"""
+
+        while True:
+            # Watch for keyboard mouse and events.
+            self._check_events()
+            self.ship.update()
+            self.bullets.update()
+
+            self._update_screen()
+
+
+
+    def _check_events(self):
+        """Respond to key presses  an mouse events."""
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                self._check_keydown_events(event)
+            elif event.type == pygame.KEYUP:
+                self._check_keyup_events(event)
+
+
+    def _check_keydown_events(self, event):
+        if event.key == pygame.K_RIGHT:
+            # Move the ship to the right.
+            self.ship.moving_right = True
+        elif event.key == pygame.K_LEFT:
+            # Move the ship to the left
+            self.ship.moving_left = True
+            self.ship.rect.x -= 1
+        elif event.key == pygame.K_q:
+            sys.exit()
+        elif event.key == pygame.K_SPACE:
+            self._fire_bullet()
+
+    def _check_keyup_events(self, event):
+        if event.key == pygame.K_RIGHT:
+            self.ship.moving_right = False
+        elif event.key == pygame.K_LEFT:
+            self.ship.moving_left = False
+
+    def _update_screen(self):
+        # Redraw the screen during each pass trough the loop.
+        self.screen.fill(self.settings.bg_color)
+        self.ship.blitme()
+
+        for bullet in self.bullets.sprites():
+            bullet.draw_bullet()
+
+        # Make  the most recently drawn screen visible .
+        pygame.display.flip()
+
+    def _fire_bullet(self):
+        """Create a new bullet and add it to the bullets group"""
+        new_bullet = Bullet(self)
+        self.bullets.add(new_bullet)
+
+if __name__ == '__main__':
+    #   Make a game instance, and run the game
+    ai = AlienInvasion()
+    ai.run_game()
---
+++ bullet.py
@@ -0,0 +1,35 @@
+import pygame
+from pygame.sprite import Sprite
+
+
+class Bullet(Sprite):
+    """A class to manage bullets fired from the ship"""
+
+
+    def __init__(self, ai_game):
+        """Create a bullet object at the ship's current position"""
+        super().__init__()
+        self.screen = ai_game.screen
+        self.settings = ai_game.settings
+        self.color = self.settings.bullet_color
+
+
+        # Create bullet rect at (0, 0) and then set the current posittion
+        self.rect = pygame.Rect(0, 0, self.settings.bullet_width,
+                                self.settings.bullet_height)
+        self.rect.midtop = ai_game.ship.rect.midtop
+
+        # Store the bullet's position as a decimal value
+        self.y  = float(self.rect.y)
+
+    def update(self):
+        """Move the bullet up to the screen."""
+        # Update the decimal position of the bullet
+        self.y -= self.settings.bullet_speed
+        # Update bullet position
+        self.rect.y = self.y
+
+
+    def draw_bullet(self):
+        """Drau the bullet on the scren"""
+        pygame.draw.rect(self.screen, self.color, self.rect)
